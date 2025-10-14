ee5dbe321c5e40a9c28ac070ef120a1b733863d6
Belle Beauterre-Babbillard <26629952+beauterre@users.noreply.github.com>
2025-10-14 23:35:01 +0200

    minus one

--- new_script.js
+++ new_script.js
@@ -27,6 +27,7 @@ function dayDifference(date1, date2) {
   console.log("dag:"+dag);
    // dag is a string like "14/10/2025" (dd/mm/yyyy)
   const daynr = dayDifference( dateObj,new Date("2025-09-30")); // pass Date objects
+  daynr-=1;
   console.log("daynr", daynr); console.log("daynr"+daynr);
   // Format date in Dutch style
   const monthsNL = ["januari","februari","maart","april","mei","juni","juli","augustus","september","oktober","november","december"];
